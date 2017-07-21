71bcd697917481bc478e78a5a307b4b626efe1db
David M Sibley <sibley@wisc.edu>
2017-07-21 11:43:05 -0500

    initial proxying

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-// var http = require('http');
-var httpProxy = require('http-proxy');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-httpProxy.createProxyServer({target:'https://predev.my.wisc.edu/'}).listen(8080);
+const proxy = httpProxy.createProxyServer({
+  target:'https://predev.my.wisc.edu:443', 
+  secure: false,
+  changeOrigin: true,
+  autoRewrite: true
+});
+
+proxy.on('proxyRes', function(proxyRes, req, res, options){
+  const headers = proxyRes.headers;
+  for (let header in headers) {
+    if (header && headers[header] && 'string' === typeof headers[header]) {
+      headers[header] = headers[header].replace(/https:\/\/predev\.my\.wisc\.edu/g, "http://localhost:8080");
+    }
+  }
+  for (let header in headers) {
+    console.log(header);
+    console.log(headers[header]);
+  }
+});
+
+const server = http.createServer(function(req, res) {
+  proxy.web(req, res);
+});
+
+server.listen(8080);
