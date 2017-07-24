499d21dc7255f320be50e81942af1f3cced0cf9f
David M Sibley <sibley@wisc.edu>
2017-07-24 10:39:32 -0500

    adding local passthrough

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const connect = require('connect');
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const httpProxy = require('http-proxy');
-const connect = require('connect');
+const https = require('https');
+const { URL } = require('url');
 
 const sslConfig = {
   key: fs.readFileSync('localhost-private.pem'),
@@ -12,7 +13,13 @@ const sslConfig = {
 http.createServer(function (req, res) {
     res.writeHead(301, { 'Location': 'https://localhost:8443' + req.url });
     res.end();
-}).listen(8080);
+}).listen(8081);
+
+const localProxy = httpProxy.createProxyServer({
+  target:'http://localhost:8080',
+  changeOrigin: true,
+  autoRewrite: true
+});
 
 const predevProxy = httpProxy.createProxyServer({
   target:'https://predev.my.wisc.edu', 
@@ -20,18 +27,23 @@ const predevProxy = httpProxy.createProxyServer({
   changeOrigin: true,
   autoRewrite: true
 });
-predevProxy.on('proxyRes', function(proxyRes, req, res, options){
+predevProxy.on('proxyRes', function(proxyRes, req, res, options) {
   const header = 'location';
   const headers = proxyRes.headers;
   if (header && headers[header] && 'string' === typeof headers[header]) {
-    headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, "localhost:8443");
-    headers[header] = headers[header].replace(/logintest\.wisc\.edu/g, "localhost:8444");
+    headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, 'localhost:8443');
+    headers[header] = headers[header].replace(/logintest\.wisc\.edu/g, 'localhost:8444');
   }
 });
 
 const predevConnect = connect();
-predevConnect.use(function(req, res) {
-  predevProxy.web(req, res);
+predevConnect.use(function(req, res, next) {
+  const inUrl = new URL(req.url, 'https://localhost:8443/');
+  if (inUrl.pathname.startsWith('/web')) {
+    localProxy.web(req, res);
+  } else {
+    predevProxy.web(req, res);
+  }
 });
 
 https.createServer(sslConfig, predevConnect).listen(8443);
@@ -43,12 +55,12 @@ const loginProxy = httpProxy.createProxyServer({
   changeOrigin: true,
   autoRewrite: true
 });
-loginProxy.on('proxyRes', function(proxyRes, req, res, options){
+loginProxy.on('proxyRes', function(proxyRes, req, res, options) {
   const header = 'location';
   const headers = proxyRes.headers;
   if (header && headers[header] && 'string' === typeof headers[header]) {
-    headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, "localhost:8443");
-    headers[header] = headers[header].replace(/logintest\.wisc\.edu/g, "localhost:8444");
+    headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, 'localhost:8443');
+    headers[header] = headers[header].replace(/logintest\.wisc\.edu/g, 'localhost:8444');
   }
 });
 
@@ -56,7 +68,7 @@ const loginConnect = connect();
 loginConnect.use(function(req, res, next) {
   let _write = res.write;
   res.write = function(data) {
-    _write.call(res, data.toString().replace(/predev\.my\.wisc\.edu/g, "localhost:8443"));
+    _write.call(res, data.toString().replace(/predev\.my\.wisc\.edu/g, 'localhost:8443'));
   };
   next();
 });
