4df7c6e45b7fe7380abd27a525e9cabaa3043f68
David M Sibley <sibley@wisc.edu>
2017-07-24 09:18:59 -0500

    adding connect

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const httpProxy = require('http-proxy');
+const connect = require('connect');
+
+const sslConfig = {
+  key: fs.readFileSync('localhost-private.pem'),
+  cert: fs.readFileSync('localhost-public.pem')
+};
 
 http.createServer(function (req, res) {
     res.writeHead(301, { 'Location': 'https://localhost:8443' + req.url });
@@ -14,7 +20,6 @@ const predevProxy = httpProxy.createProxyServer({
   changeOrigin: true,
   autoRewrite: true
 });
-
 predevProxy.on('proxyRes', function(proxyRes, req, res, options){
   const header = 'location';
   const headers = proxyRes.headers;
@@ -24,29 +29,32 @@ predevProxy.on('proxyRes', function(proxyRes, req, res, options){
   }
 });
 
-const server = https.createServer({
-  key: fs.readFileSync('localhost-private.pem'),
-  cert: fs.readFileSync('localhost-public.pem')
-}, function(req, res) {
+const predevConnect = connect();
+predevConnect.use(function(req, res) {
   predevProxy.web(req, res);
 });
 
-server.listen(8443);
+https.createServer(sslConfig, predevConnect).listen(8443);
+
 
 const loginProxy = httpProxy.createProxyServer({
   target:'https://logintest.wisc.edu',
-  ssl: {
-    key: fs.readFileSync('localhost-private.pem'),
-    cert: fs.readFileSync('localhost-public.pem')
-  },
   secure: false,
   changeOrigin: true,
   autoRewrite: true
-}).on('proxyRes', function(proxyRes, req, res, options){
+});
+loginProxy.on('proxyRes', function(proxyRes, req, res, options){
   const header = 'location';
   const headers = proxyRes.headers;
   if (header && headers[header] && 'string' === typeof headers[header]) {
     headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, "localhost:8443");
     headers[header] = headers[header].replace(/logintest\.wisc\.edu/g, "localhost:8444");
   }
-}).listen(8444);
+});
+
+const loginConnect = connect();
+loginConnect.use(function(req, res) {
+  loginProxy.web(req, res);
+})
+
+https.createServer(sslConfig, loginConnect).listen(8444);
