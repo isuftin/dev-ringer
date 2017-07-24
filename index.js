30f2319b32c6b4a90fdc868df80cd3400b3f9302
David M Sibley <sibley@wisc.edu>
2017-07-24 09:10:35 -0500

    adding loginProxy

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
 const httpProxy = require('http-proxy');
 
-const proxy = httpProxy.createProxyServer({
-  target:'https://predev.my.wisc.edu:443', 
+http.createServer(function (req, res) {
+    res.writeHead(301, { 'Location': 'https://localhost:8443' + req.url });
+    res.end();
+}).listen(8080);
+
+const predevProxy = httpProxy.createProxyServer({
+  target:'https://predev.my.wisc.edu', 
   secure: false,
   changeOrigin: true,
   autoRewrite: true
 });
 
-proxy.on('proxyRes', function(proxyRes, req, res, options){
+predevProxy.on('proxyRes', function(proxyRes, req, res, options){
+  const header = 'location';
   const headers = proxyRes.headers;
-  for (let header in headers) {
-    if (header && headers[header] && 'string' === typeof headers[header]) {
-      headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, "localhost:8443");
-    }
-  }
-  for (let header in headers) {
-    console.log(header);
-    console.log(headers[header]);
+  if (header && headers[header] && 'string' === typeof headers[header]) {
+    headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, "localhost:8443");
+    headers[header] = headers[header].replace(/logintest\.wisc\.edu/g, "localhost:8444");
   }
 });
 
@@ -26,7 +28,25 @@ const server = https.createServer({
   key: fs.readFileSync('localhost-private.pem'),
   cert: fs.readFileSync('localhost-public.pem')
 }, function(req, res) {
-  proxy.web(req, res);
+  predevProxy.web(req, res);
 });
 
 server.listen(8443);
+
+const loginProxy = httpProxy.createProxyServer({
+  target:'https://logintest.wisc.edu',
+  ssl: {
+    key: fs.readFileSync('localhost-private.pem'),
+    cert: fs.readFileSync('localhost-public.pem')
+  },
+  secure: false,
+  changeOrigin: true,
+  autoRewrite: true
+}).on('proxyRes', function(proxyRes, req, res, options){
+  const header = 'location';
+  const headers = proxyRes.headers;
+  if (header && headers[header] && 'string' === typeof headers[header]) {
+    headers[header] = headers[header].replace(/predev\.my\.wisc\.edu/g, "localhost:8443");
+    headers[header] = headers[header].replace(/logintest\.wisc\.edu/g, "localhost:8444");
+  }
+}).listen(8444);
